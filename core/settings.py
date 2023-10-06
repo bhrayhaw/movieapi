251baa297c9e6e98e0daf6fe58bba055041673b3
bhrayhaw <elijahapreko@gmail.com>
2023-10-06 02:52:53 +0000

    changed authentication to JWT

--- core/settings.py
+++ core/settings.py
@@ -130,6 +130,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
--- user_app/urls.py
+++ user_app/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from .views import registrationview, logoutview
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
     path('register/', registrationview, name='register'),
     path('logout/', logoutview, name='logout'),
+    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    
+    
 ]
